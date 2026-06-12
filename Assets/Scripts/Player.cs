7ec897f1f74ef731d2ea45f6f3f5a6abe5aea907
Joa-e10 <joaquin.ferreyra.e@gmail.com>
2026-06-11 22:58:12 -0300

    Script de Player
    Se crea el script "Player" con la configuración de la "camaraFreeLook"y un sistema de movimiento para el player en red

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+using System.Globalization;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class Player : NetworkBehaviour
+{
+
+    private CharacterController _characterController;
+    [SerializeField]private PlayerInput _playerInput;
+   [SerializeField]private GameObject _cam;
+    private Vector2 _input;
+    private float _speed = 5f;
+    private float _yVelocity;
+    private float _gravity = -9.81f;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        _playerInput.enabled = IsOwner;
+        _cam.SetActive(IsOwner);
+    }
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        Vector3 moveDirection = GetCameraRelativeDirection();
+
+        RotateCharacter(moveDirection);
+        moveDirection = ApplyGravity(moveDirection);
+        MoveCharacter(moveDirection);
+    }
+
+    private Vector3 GetCameraRelativeDirection()
+    {
+        Transform cam = Camera.main.transform;
+
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        return camRight * _input.x + camForward * _input.y;
+    }
+
+    private Vector3 ApplyGravity(Vector3 moveDirection)
+    {
+        if (_characterController.isGrounded && _yVelocity < 0)
+        {
+            _yVelocity = -2f;
+        }
+
+        _yVelocity += _gravity * Time.deltaTime;
+        moveDirection.y = _yVelocity;
+
+        return moveDirection;
+    }
+    private void RotateCharacter(Vector3 moveDirection)
+    {
+        if (moveDirection.magnitude <= 0.1f) return;
+
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+    }
+
+    private void MoveCharacter(Vector3 moveDirection)
+    {
+        _characterController.Move(moveDirection * _speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _input = value.Get<Vector2>();
+    }
+}
