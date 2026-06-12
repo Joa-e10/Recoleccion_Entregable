c4b0a8f166edd9b0ed61bfddcdca36d54afc7bbd
Joa-e10 <joaquin.ferreyra.e@gmail.com>
2026-06-11 23:42:59 -0300

    ServerManager
    Se integro un script manager para iniciar la sesión en red.

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,29 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ServerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _pointPrefab;
+    public void InstantiateObject()
+    {
+        GameObject item = Instantiate(_pointPrefab);
+
+        item.GetComponent<NetworkObject>().Spawn();
+        Debug.Log("Spawnearon objetos en escena");
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            NetworkManager.Singleton.StartHost(); //Iniciamos el Host
+            InstantiateObject();
+        }
+        if (Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            NetworkManager.Singleton.StartClient(); //Iniciamos como client
+
+        }
+    }
+}
